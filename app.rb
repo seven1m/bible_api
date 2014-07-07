dac8c7d18d58996d298e9b5e0540757ce1281311
Tim Morgan <tim@timmorgan.org>
2014-07-06 22:38:36 -0500

    Return error if not all verses can be found.

--- app.rb
+++ app.rb
@@ -6,12 +6,13 @@ Bundler.require
 DB = Sequel.connect(ENV['BIBLE_API_DB'])
 
 def get_verse_id(ref)
-  DB[
+  record = DB[
     'select id from verses where book_id = ? and chapter = ? and verse = ?',
     ref[:book],
     ref[:chapter],
     ref[:verse]
-  ].first[:id]
+  ].first
+  record ? record[:id] : nil
 end
 
 def get_verses(ranges)
@@ -19,7 +20,11 @@ def get_verses(ranges)
   ranges.each do |(ref_from, ref_to)|
     start_id = get_verse_id(ref_from)
     stop_id  = get_verse_id(ref_to)
-    all += DB['select * from verses where id between ? and ?', start_id, stop_id].to_a
+    if start_id and stop_id
+      all += DB['select * from verses where id between ? and ?', start_id, stop_id].to_a
+    else
+      return nil
+    end
   end
   all
 end
@@ -40,23 +45,28 @@ end
 get '/:ref' do
   ref = params[:ref].gsub(/\+/, ' ')
   if ranges = BibleRef::Reference.new(ref).ranges
-    verses = get_verses(ranges).map do |v|
+    if verses = get_verses(ranges)
+      verses.map! do |v|
+        {
+          book_id:   v[:book_id],
+          book_name: v[:book],
+          chapter:   v[:chapter],
+          verse:     v[:verse],
+          text:      v[:text]
+        }
+      end
       {
-        book_id:   v[:book_id],
-        book_name: v[:book],
-        chapter:   v[:chapter],
-        verse:     v[:verse],
-        text:      v[:text]
-      }
+        reference: ref,
+        verses: verses,
+        text: verses.map { |v| v[:text] }.join,
+        translation_id: 'WEB',
+        translation_name: 'World English Bible',
+        translation_note: 'The World English Bible, a Modern English update of the American Standard Version of the Holy Bible, is in the public domain. Copy and publish it freely.'
+      }.to_json
+    else
+      status 404
+      { error: 'not found' }.to_json
     end
-    {
-      reference: ref,
-      verses: verses,
-      text: verses.map { |v| v[:text] }.join,
-      translation_id: 'WEB',
-      translation_name: 'World English Bible',
-      translation_note: 'The World English Bible, a Modern English update of the American Standard Version of the Holy Bible, is in the public domain. Copy and publish it freely.'
-    }.to_json
   else
     status 404
     { error: 'not found' }.to_json
