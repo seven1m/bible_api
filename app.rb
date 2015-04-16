64a305c7156ec4bb56805b375644e92bd39f6a4d
Tim Morgan <tim@timmorgan.org>
2015-04-15 21:18:32 -0500

    Add new homepage

--- app.rb
+++ app.rb
@@ -33,35 +33,9 @@ def get_verses(ranges, translation_id)
 end
 
 get '/' do
-  content_type 'application/json; charset=utf-8'
-  translations = DB['select identifier, name from translations'].each_with_object({}) { |t, h| h[t[:identifier]] = t[:name] }
-  response = {
-    url: 'http://bible-api.com',
-    description: 'RESTful JSON API for querying bible passages from the World English Bible.',
-    creator: {
-      name: 'Tim Morgan',
-      url: 'https://timmorgan.org',
-      twitter_url: 'https://twitter.com/seven1m'
-    },
-    source_code: {
-      url: 'https://github.com/seven1m/bible_api',
-      bugs: 'https://github.com/seven1m/bible_api/issues'
-    },
-    examples: {
-      'single verse'          => 'http://bible-api.com/john+3:16',
-      'verse range'           => 'http://bible-api.com/romans+12:1-2',
-      'the kitchen sink'      => 'http://bible-api.com/romans+12:1-2,5-7,9,13:1-9&10',
-      'jsonp'                 => 'http://bible-api.com/john+3:16?callback=func',
-      'unknown'               => 'http://bible-api.com/mormon',
-      'different translation' => 'http://bible-api.com/john+3:16?translation=RCCV'
-    },
-    translations: translations,
-    notes: [
-      "JSONView is good JSON-viewing plugin for your browser.",
-      "You don't have to use plus (+) signs for spaces. We did here so JSONView will auto-link them.",
-    ]
-  }
-  JSONP response
+  @translations = DB['select identifier, name from translations'].each_with_object({}) { |t, h| h[t[:identifier]] = t[:name] }
+  @host = request.env['REQUEST_URI'].split('?').first
+  erb :index
 end
 
 get '/:ref' do
