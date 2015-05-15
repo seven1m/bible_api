f46e9a69e535ef4b02e03550c6cb683ed95fef5a
Tim Morgan <tim@timmorgan.org>
2015-05-14 21:50:39 -0500

    Support book names in target language (portuguese only for now)

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'bundler'
 require 'json'
 
@@ -7,13 +8,13 @@ DB = Sequel.connect(ENV['BIBLE_API_DB'], charset: 'utf8')
 
 set :protection, except: [:json_csrf]
 
-def get_verse_id(ref, translation_id)
+def get_verse_id(ref, translation_id, last = false)
   record = DB[
-    'select id from verses where book_id = ? and chapter = ? and verse = ? and translation_id = ?',
-    ref[:book],
-    ref[:chapter],
-    ref[:verse],
-    translation_id
+    "select id from verses " \
+    "where book_id = :book and chapter = :chapter #{ref[:verse] ? 'and verse = :verse' : ''} " \
+    "and translation_id = :translation_id " \
+    " #{last ? 'order by id desc' : ''} limit 1",
+    ref.update(translation_id: translation_id)
   ].first
   record ? record[:id] : nil
 end
@@ -22,7 +23,7 @@ def get_verses(ranges, translation_id)
   all = []
   ranges.each do |(ref_from, ref_to)|
     start_id = get_verse_id(ref_from, translation_id)
-    stop_id  = get_verse_id(ref_to, translation_id)
+    stop_id  = get_verse_id(ref_to, translation_id, :last)
     if start_id and stop_id
       all += DB['select * from verses where id between ? and ?', start_id, stop_id].to_a
     else
@@ -41,13 +42,13 @@ end
 get '/:ref' do
   content_type 'application/json; charset=utf-8'
   ref_string = params[:ref].gsub(/\+/, ' ')
-  ref = BibleRef::Reference.new(ref_string)
   translation = DB['select * from translations where identifier = ?', params[:translation] || 'WEB'].first
   unless translation
     status 404
     response = { error: 'translation not found' }
     return JSONP(response)
   end
+  ref = BibleRef::Reference.new(ref_string, language: translation[:language_code])
   if ranges = ref.ranges
     if verses = get_verses(ranges, translation[:id])
       verses.map! do |v|
