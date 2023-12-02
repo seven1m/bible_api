15795fe90a22ee23b7892883662f2f9ea441c00b
Tim Morgan <tim@timmorgan.org>
2023-12-01 18:22:17 -0600

    Use Puma instead of Thin

---
+++ config/puma.rb
@@ -0,0 +1,8 @@
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads threads_count, threads_count
+
+preload_app!
+
+port        ENV.fetch("PORT") { 5000 }
+environment ENV.fetch("RACK_ENV") { "development" }
