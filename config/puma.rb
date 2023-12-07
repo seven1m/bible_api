2aaf27af63c140d7dc756a8ed8954c1fa6d9fd38
Tim Morgan <tim@timmorgan.org>
2023-12-07 08:23:07 -0600

    Lower thread count

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 workers ENV.fetch('WEB_CONCURRENCY') { 2 }
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 3 }
 threads threads_count, threads_count
 
 preload_app!
